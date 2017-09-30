008d09fc7a7dc4bce500b40d81872a0e83983911
jackdund <jack.dundas@gmail.com>
2017-09-30 00:01:02 -0700

    finished word parser

--- src/shakespeare.cpp
+++ src/shakespeare.cpp
@@ -9,6 +9,15 @@
 
 #include "word_count.h"
 
+
+//1) How can you test your program without needing to manually go through all the dialogue in Shakespeare’s plays ?
+	//Verify a small section of dialogue and make sure it adds up. 
+
+//2) Has writing this code multithreaded helped in any way ?
+	//Yes -- 
+
+// 3) As written, if a character in one play has the same name as a character in another – e.g.King – it will threat them as the same and artificially increase the word count.How can you modify your code to treat them as separate, but still store all characters in the single map(you do not need to implement this… just think about how you would do it) ?
+
 /**
  * Checks if the line specifies a character's dialogue, returning
  * the index of the start of the dialogue.  If the
@@ -71,10 +80,6 @@ void count_character_words(const std::string& filename,
                            std::mutex& mutex,
                            std::map<std::string,int>& wcounts) {
 
-  //===============================================
-  //  IMPLEMENT THREAD SAFETY IN THIS METHOD
-  //===============================================
-
   std::string line;  // for storing each line read from the file
   std::ifstream file (filename);
 
@@ -87,15 +92,24 @@ void count_character_words(const std::string& filename,
     while ( std::getline (file,line) ) {
 
       int idx = is_dialogue_line(line, character);
+	  
+	  //index will be -1 if the line is not a dialogue line, and character will be empty if there is no character 
       if (idx > 0 && !character.empty()) {
 
         int nwords = word_count(line, idx);
 
         // add character if doesn't exist, otherwise increment count
-
-        //=================================================
-        // YOUR JOB TO ADD WORD COUNT INFORMATION TO MAP
-        //=================================================
+		//find returns an iterator - called it
+		auto it = wcounts.find(character);
+		if (it == wcounts.end()) {
+			// not found, so add it
+			
+			wcounts.insert({ character, nwords});
+		}
+		else {
+			// found, so add the nwords to total word count
+			it->second += nwords;
+		}
 
       } else {
         character = "";  // reset character
@@ -107,6 +121,53 @@ void count_character_words(const std::string& filename,
 
 }
 
+void count_character_words_threadsafe(const std::string& filename, std::mutex& mutex, std::map<std::string, int>& wcounts) {
+
+	std::string line;  // for storing each line read from the file
+	std::ifstream file(filename);
+
+	// read contents of file if open
+	if (file.is_open()) {
+
+		std::string character = "";  // empty character to start
+
+									 // line by line
+		while (std::getline(file, line)) {
+
+			int idx = is_dialogue_line(line, character);
+
+			//index will be -1 if the line is not a dialogue line, and character will be empty if there is no character 
+			if (idx > 0 && !character.empty()) {
+
+				int nwords = word_count(line, idx);
+
+				// add character if doesn't exist, otherwise increment count
+				//find returns an iterator - called it
+				auto it = wcounts.find(character);
+				if (it == wcounts.end()) {
+					// not found, so add it
+					while (!mutex.try_lock()) {}
+						wcounts.insert({ character, nwords });
+				}
+				else {
+					// found, so add the nwords to total word count
+					while (!mutex.try_lock()) {}
+					it->second += nwords;
+				}
+				mutex.unlock();
+
+			}
+			else {
+				character = "";  // reset character
+			}
+
+		}
+		file.close();  // close file
+	}
+
+}
+
+
 /**
  * Comparator, orders by number decreasing, then by name
  * @param p1 first pair
@@ -115,10 +176,6 @@ void count_character_words(const std::string& filename,
  */
 bool wc_greater_than(std::pair<std::string,int>& p1, std::pair<std::string,int>& p2) {
 
-  //===============================================
-  // YOUR IMPLEMENTATION HERE TO ORDER p1 AND p2
-  //===============================================
-
 	//if p1 speaks more, return true, signifying p1
 	if (p1.second > p2.second)
 		return true;
@@ -126,7 +183,7 @@ bool wc_greater_than(std::pair<std::string,int>& p1, std::pair<std::string,int>&
 	//if p1 and p2 speak the same amount, then return true (signifying p1 should come first)
 	// if p1's name comes first in the alphabet
 	if (p1.second == p2.second)
-		if (p1.first > p2.first)
+		if (p1.first < p2.first)
 			return true;
 
   return false;
@@ -175,13 +232,51 @@ int main() {
   //=============================================================
   // YOUR IMPLEMENTATION HERE TO COUNT WORDS IN MULTIPLE THREADS
   //=============================================================
+  std::vector<std::thread> threads;
+
+  
+
+  auto t1 = std::chrono::high_resolution_clock::now(); //sets start time
+
+													   //concurrent implementation 
+	  for (int i = 0; i < filenames.size(); i++) {
+		  count_character_words(filenames[i], mutex, wcounts);
+	  }
+
+  auto t2 = std::chrono::high_resolution_clock::now(); //sets end time
+  long duration_single = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count(); //converts run time to ms
+
+  
+  
+  t1 = std::chrono::high_resolution_clock::now(); //sets start time
+
+	  for (int i = 0; i < filenames.size(); i++) {
+		  
+		  threads.push_back(std::thread( count_character_words_threadsafe , std::ref(filenames[i]), std::ref(mutex), std::ref(wcounts) ));
+	  };
+	  
+
+	  for (int i = 0; i < filenames.size(); i++) {
+		  threads[i].join();
+	  }
+
+  t2 = std::chrono::high_resolution_clock::now(); //sets end time
+  long duration_multi = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count(); //converts run time to ms
+
+ // std::cout << "time single: " << 0 << "\ttime multi: " << duration_multi << std::endl;
+
+
+
 
   auto sorted_wcounts = sort_characters_by_wordcount(wcounts);
 
   // results
   for (const auto& entry : sorted_wcounts) {
-    std::cout << entry.first << ", " << entry.second << std::endl;
+    std::cout << entry.first << "\t\t" << entry.second << std::endl;
   }
+
+  std::cout << "time single: " << duration_single << "\ttime multi: " << duration_multi << std::endl;
+
 
   std::cout << std::endl << "Press ENTER to continue..." << std::endl;
   std::cin.get();
